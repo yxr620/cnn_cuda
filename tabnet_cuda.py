0ce6a0e7b05372cfff51ac965bc44224628f4a7d
yxr620 <yxr620@126.com>
2023-08-21 16:53:22 +0800

    add timer to tabnet

--- tabnet_cuda.py
+++ tabnet_cuda.py
@@ -1,33 +1,51 @@
 import torch
 from pytorch_tabnet.tab_model import TabNetRegressor
 import numpy as np
+import time
 
 device_name = "cuda" if torch.cuda.is_available() else "cpu"
-device_name = "cpu"
+device_name = 'cpu'
 print(device_name)
-# 创建特征和标签的示例数据
-X_train = np.array(torch.randn((100, 84)))
-y_train = np.array(torch.randn((100, 1)))
 
-# 定义TabNetRegressor模型
-# implement the following model using pytorch_tabnet, and place the model on gpu
-# StackedTabNetRegressor(feature_columns=None,
-#                        num_regressors=1,
-#                        num_layers=2,
-#                        num_features=84,
-#                        feature_dim=17,
-#                        output_dim=16,
-#                        num_decision_steps=1)
+
+# random data for train and validation
+train_data = torch.randn(1000000, 84).numpy()
+train_labels = torch.randn(1000000, 1).numpy()
+val_data = torch.randn(2000, 84).numpy()
+val_labels = torch.randn(2000, 1).numpy()
 
 
 # gpu model
-model = TabNetRegressor(n_d=17, n_a=17, n_steps=1, gamma=1.5, n_independent=2, n_shared=2, seed=0, device_name=device_name)
+model = TabNetRegressor(
+    n_d=17,
+    n_a=17,
+    n_steps=1,
+    gamma=1.5,
+    n_independent=2,
+    n_shared=2,
+    seed=0,
+    device_name=device_name
+)
 
-# 训练模型
-model.fit(X_train=X_train, y_train=y_train, max_epochs=100)
+# train_data = train_data.to(device_name)
+# train_labels = train_labels.to(device_name)
+# val_data = val_data.to(device_name)
+# val_labels = val_labels.to(device_name)
+
+
+# train model
+start = time.time()
+model.fit(
+    X_train=train_data,
+    y_train=train_labels,
+    eval_set=[(val_data, val_labels)],
+    max_epochs=10
+)
+end = time.time()
+
+# model.fit(X_train=X_train, y_train=y_train, max_epochs=100)
 
 # 使用模型进行预测
 X_test = torch.randn((10, 84))
 predictions = model.predict(X_test)
-
-print(predictions)
+print(f"training on {device_name}: {end - start} seconds")
